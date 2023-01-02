d046fce8f1a4f79450b1c9472f93098c6dd9c78f
ArthurAthayde <arthur.athaydee@gmail.com>
2023-01-02 08:20:53 -0300

    webw base

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,28 @@
 /* Desenvolva sua lógica aqui... */
+
+function renderCard(array){
+const cards = document.querySelector('.cards__list');
+const card = document.createElement('li');
+const cardTitle = document.createElement('h3');
+const infoContainer = document.createElement('div');
+const cardCompany = document.createElement('span'); 
+const cardCity = document.createElement('span');
+const cardDescription = document.createElement('p');
+const cardModality = document.createElement('span');
+const cardBtn = document.createElement('button')
+
+}
+
+{/* <ul class="cards__list ">
+          <li class="card">
+            <h3 class="card__title">Pessoa desenvolvedora front-end - React JS</h3>
+            <div class="info__container">
+              <span class="card__company">Kenzie Academy</span>
+              <span class="card__city">Curitiba</span>
+            </div>
+            <p class="card__description">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem
+              Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            <span class="card__modality">Home Office</span>
+            <button class="card__button">Candidatar</button>
+          </li> */}
